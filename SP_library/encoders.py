763fd067bd2b35cedc238bb70f7707dabdd48caa
hxs7174 <hxs7174@g.rit.edu>
2018-10-06 21:57:11 -0400

    Adding copyright from Numenta for encoders.py as I had previously forgotten to

--- SP_library/encoders.py
+++ SP_library/encoders.py
@@ -1,6 +1,31 @@
 """
 Author: Humza Syed
 Description: File containing encoder(s) for usage in HTM
+
+Nearly all code utilized for this file was from:
+https://github.com/numenta/nupic/blob/master/src/nupic/encoders/scalar.py 
+Some modifications were made for my own experiments, however I am putting the
+Numenta disclaimer as they should receive most of the credit for the code
+
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013-2015, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+
 """
 
 import numpy as np
